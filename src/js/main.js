6d76e68d82a50b6699c54208c5a15a5df505de97
LesyaShultz <ambrella911911911@gmail.com>
2021-11-06 17:29:55 +0400

    add files

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-    $('.js-nav-opener').on('click', function () {
-        if($('.nav').hasClass('active')) {
-            $('.nav').removeClass('active');
-        } else {
-            $('.nav').addClass('active');
-        }
-    })
-});
